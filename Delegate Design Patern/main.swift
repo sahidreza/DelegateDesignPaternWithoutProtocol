bf65893fa87802f14e5627d4463aaa357e7cad54
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-21 17:02:13 +0530

    First Commit

--- Delegate Design Patern/main.swift
+++ Delegate Design Patern/main.swift
@@ -5,7 +5,43 @@
 //  Created by Sahid Reza on 21/12/22.
 //
 
-import Foundation
 
-print("Hello, World!")
+class Developer
+{
+     var managerReference: Manager?
+
+    func developerWillAddTwoNumber(a: Int, b: Int)
+    {
+        let result = a + b
+        managerReference?.didFinishAddingTwoNumbers(result: result)
+    }
+    
+    
+}
+
+class Manager {
+    var developer: Developer?
+
+    init(developer: Developer) {
+        self.developer = developer
+        developer.managerReference = self
+        
+        
+    }
+    
+    func addTwoNumber(x: Int, y: Int)
+    {
+        developer?.developerWillAddTwoNumber(a: x, b: y)
+    }
+
+    func didFinishAddingTwoNumbers(result: Int)
+    {
+        print("result of addition = \(result)")
+    }
+
+   
+}
+
+let objManager = Manager(developer: Developer())
+objManager.addTwoNumber(x: 10, y: 10)
 
